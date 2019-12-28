ecde7421c681d5f6f3514bcd67d68c46ff9785f8
ntut108368527 <philip.chiang@tatung.com>
2019-12-28 12:02:32 +0800

    {{WEB}} 	The new comment inert ,then the res_star will change

--- web/DB_Project/create_commit_finish.php
+++ web/DB_Project/create_commit_finish.php
@@ -23,7 +23,13 @@
         $row = $o->fetch_assoc();
         return $row['COMTR_NUM'];
     }
-
+    function avg_star($id){
+        include("mysql_connect.inc.php");
+        $select = "select RST_STAR From rst_table Where RST_NUM = '$id'";
+        $o = $conn->query($select);
+        $row = $o->fetch_assoc();
+        return $row['RST_STAR'];
+    }
     function COM_LINE($id){
         include("mysql_connect.inc.php");
         $select = "select count(COM_LINE) from comment_line where RST_NUM = '$id'";
@@ -32,6 +38,7 @@
         // print_r($row);
         return $row['count(COM_LINE)'];
     }
+    // echo avg_star($id);
     echo $id;
     echo '<br>';
     echo COM_LINE($id)+1;
@@ -44,12 +51,15 @@
     echo $commit;
     echo '<br>';
     echo COMTR_NUM($ID);
+    $avgStar = number_format((avg_star($id) + $star)/2,1);
     $num = COMTR_NUM($ID);
     $data="select* from comment_line";
     $output = $conn->query($data);
     $row = $output->fetch_assoc();
     $sql = "INSERT INTO comment_line(RST_NUM,COM_LINE,COM_DATE_TIME,COM_STAR,COM_MSG,COMTR_NUM) VALUES('$id','$line','$time','$star','$commit','$num')";
     $conn->query($sql);
+    $insert = "UPDATE rst_table SET RST_STAR = '$avgStar' WHERE RST_NUM = '$id'";
+    $conn->query($insert);
     echo 'Success!';
     echo '<form method = "POST">';
     echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
